bdf225de11e5fa44714655ac24ab0780250a6086
Keyur Vithlani <vithlanikeyur27@gmail.com>
2025-10-05 10:57:33 +0200

    Added Ok and Cancel button in new layout to make it look nicer

--- add_checkbox_dialog.py
+++ add_checkbox_dialog.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QDialog, QLineEdit, QVBoxLayout, QPushButton
+from PySide6.QtWidgets import QDialog, QLineEdit, QVBoxLayout, QHBoxLayout, QPushButton
 
 
 class AddCheckboxDialog(QDialog):
@@ -9,16 +9,20 @@ class AddCheckboxDialog(QDialog):
 
         self.layout = QVBoxLayout()
 
-        self.label_line_edit = QLineEdit()
-        self.layout.addWidget(self.label_line_edit)
-
         # Make sure the dialog uses this layout
         self.setLayout(self.layout)
 
+        self.label_line_edit = QLineEdit()
+        self.layout.addWidget(self.label_line_edit)
+
+        self.button_layout = QHBoxLayout()
+
         self.ok_button = QPushButton("OK")
         self.ok_button.clicked.connect(self.accept)
-        self.layout.addWidget(self.ok_button)
+        self.button_layout.addWidget(self.ok_button)
 
         self.cancel_button = QPushButton("Cancel")
         self.cancel_button.clicked.connect(self.reject)
-        self.layout.addWidget(self.cancel_button)
+        self.button_layout.addWidget(self.cancel_button)
+
+        self.layout.addLayout(self.button_layout)
