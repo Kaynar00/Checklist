2af34c09e02eeb3cb12a24899375291882ab2e70
Keyur Vithlani <vithlanikeyur27@gmail.com>
2025-10-05 10:44:29 +0200

    Add lineedit to add dialog.

--- add_checkbox_dialog.py
+++ add_checkbox_dialog.py
@@ -1,7 +1,16 @@
-from PySide6 import QtWidgets
+from PySide6.QtWidgets import QDialog, QLineEdit, QVBoxLayout
 
-class AddCheckboxDialog(QtWidgets.QDialog):
+
+class AddCheckboxDialog(QDialog):
     def __init__(self, parent=None):
         super(AddCheckboxDialog, self).__init__(parent)
         self.setWindowTitle("Add Checkbox")
         self.setModal(True)
+
+        self.layout = QVBoxLayout()
+
+        self.label_line_edit = QLineEdit()
+        self.layout.addWidget(self.label_line_edit)
+
+        # Make sure the dialog uses this layout
+        self.setLayout(self.layout)
